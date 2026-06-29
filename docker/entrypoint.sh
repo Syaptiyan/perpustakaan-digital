444a4986e8ba30e76e7ff53e97f6874be8ac7f6e
Syaptiyan <Syaptiyan@users.noreply.github.com>
2026-06-29 04:14:54 +0000

    fix: add ServerName to suppress warning

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,5 +4,8 @@ a2dismod mpm_event 2>/dev/null || true
 a2dismod mpm_worker 2>/dev/null || true
 a2enmod mpm_prefork 2>/dev/null || true
 
+# Set ServerName to suppress warning
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
 # Start Apache
 exec apache2-foreground
